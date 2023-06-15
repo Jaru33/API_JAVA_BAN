c244c9ef7179aec583bad2b84b8bc2746c00cbf2
Jaru33 <thswlsdnjs33@gmail.com>
2023-06-15 17:15:06 +0900

    구구단완성 by -sjw

--- Gugudan/src/ezen/Gugudan.java
+++ Gugudan/src/ezen/Gugudan.java
@@ -8,6 +8,13 @@ public class Gugudan {
 		System.out.println("구구단 연습문제");
 		
 		System.out.println(2 * 1);
+		System.out.println(2 * 2);
+		System.out.println(2 * 3);
+		System.out.println(2 * 4);
+		System.out.println(2 * 5);
+		System.out.println(2 * 6);
+		System.out.println(2 * 7);
+		System.out.println(2 * 8);
 		System.out.println(2 * 9);
 		
 	}
